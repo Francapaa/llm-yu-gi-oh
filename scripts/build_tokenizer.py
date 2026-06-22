fc3b3b3745af06d71aa07fa1d7518be954df270c
Francapaa <franciscocaparruva@gmail.com>
2026-06-22 18:14:38 -0300

    feat: add bpe tokenizer

---
+++ scripts/build_tokenizer.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+DATASET_PATH = Path("data/processed/yugioh/v001/all.txt")
+CHAR_TOKENIZER = Path()
--- tokenizer.py
+++ tokenizer.py
@@ -3,15 +3,16 @@ import enum
 from pathlib import Path
 import json
 from typing import Optional
-from torch import Tensor
-
+from torch import Tensor, tensor
+from tokenizers import  Tokenizer as HFTokenizer
+from tokenizers import models, pre_tokenizers, trainers, processors, decoders
 
 CARD_START = "<card>"
 CARD_END = "</card>"
 
 
 
-class TokenizerType(enum): 
+class TokenizerType(enum.Enum): 
    CHAR = "char"
    BPE = "bpe" # byte pair encode
 
@@ -60,4 +61,42 @@ class CharacterTokenizer(Tokenizer):
         if not self.c_to_i:
             raise Exception("Forgot to load tokenizer mapping")
         return len(self.c_to_i)
-        
+
+
+
+class BPETokenizer(Tokenizer):
+    def __init__(self, mapping_path: Optional[Path] = None):
+        self.mapping_path = mapping_path
+        if self.mapping_path:
+            self.tokenizer = HFTokenizer.from_file(str(self.mapping_path))
+        else:
+            self.tokenizer = HFTokenizer(models.BPE())
+
+
+    def building_mapping( self, input_dataset_path: Path, mapping_save_path: Path, vocab_size: int):
+        self.tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(add_prefix_space=False)
+        trainer = trainers.BpeTrainer(
+            vocab_size=vocab_size, 
+            special_tokens=[CARD_START, CARD_END], 
+            initial_alphabet=pre_tokenizers.ByteLevel.alphabet(),
+        )
+        self.tokenizer.train([str(input_dataset_path)], trainer=trainer)
+        self.tokenizer.post_processor = processors.ByteLevel(trim_offsets=False)
+        self.tokenizer.decoder = decoders.ByteLevel()
+        self.tokenizer.save(str(mapping_save_path))
+        print(f"Saved tokenizer mapping to {mapping_save_path}")
+
+
+    def vocab_size(self) -> int:
+        return self.tokenizer.get_vocab_size()
+
+    def encode(self, s: str) -> int:
+        return tensor(self.tokenizer.encode(s).ids)
+
+    def decode(self, ix: Tensor) -> str:
+        return self.tokenizer.decode(ix.tolist(), skip_special_tokens=False)
+
+    def get_type(self) -> TokenizerType:
+        return TokenizerType.BPE 
+
+    
